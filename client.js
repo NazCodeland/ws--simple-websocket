648e1e4ca4fdbeeb601781648bba2055e79163e5
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-04-09 12:24:58 -0400

    client-side websocket implementation

---
+++ client.js
@@ -0,0 +1,13 @@
+
+// change 'wsAddress' to your WebSocket URL
+let wsAddress = ''
+const clientSocket = new WebSocket(wsAddress)
+
+clientSocket.addEventListener('open', () => { })
+clientSocket.addEventListener('message', () => { })
+clientSocket.addEventListener('error', () => { })
+clientSocket.addEventListener('close', () => { })
+
+
+
+
